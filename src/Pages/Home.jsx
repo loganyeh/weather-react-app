da3e6ab438a26e30e90d466afbb9f0a281d4c5ed
loganyeh <yehlogan@gmail.com>
2025-12-11 21:01:00 -0500

    delete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,6 +25,7 @@ import MainCard from "../Components/MainCard.jsx";
 import SmallCard from "../Components/SmallCard.jsx";
 import DailyForecastBlock from "../Components/DailyForecastBlock.jsx";
 import HourlyForecastTimeBlock from "../Components/HourlyForecastTimeBlock.jsx";
+import HourlyForecastBlock from "../Components/HourlyForecastBlock.jsx";
 
 function Home() {
   const [isUnitsDropdown, setIsUnitsDropdown] = useState(true);
@@ -231,35 +232,11 @@ function Home() {
             border: `2px solid ${colors.neutral700}`,
           }}
         >
-          {/* ONE HOUR BLOCK */}
-          <div className="h-14 w-full pt-2 flex justify-around items-center">
-            <h3 className="h-full w-2/4 flex justify-start items-center text-lg ml-3 whitespace-nowrap">
-              Hourly Forecast
-            </h3>
-            <h3
-              onClick={handleIsHourlyForecastDropdown}
-              className={`h-10 w-4/12 flex justify-center items-center text-sm rounded-xl hover:border-2 hover:border-white cursor-pointer
-              ${isHourlyForecastDropdown ? `` : "border-2 border-white"}
-              `}
-              style={{
-                backgroundColor: colors.neutral600,
-                // border: `2px solid ${colors.neutral700}`,
-              }}
-            >
-              Tuesday \/
-            </h3>
-          </div>
 
-          <div className="h-11/12 w-full flex flex-col justify-around items-center">
-            <HourlyForecastTimeBlock weatherImg={overcast} time={"3 PM"} temp={"20°"} />
-            <HourlyForecastTimeBlock weatherImg={partlyCloudy} time={"4 PM"} temp={"20°"} />
-            <HourlyForecastTimeBlock weatherImg={sunny} time={"5 PM"} temp={"20°"} />
-            <HourlyForecastTimeBlock weatherImg={overcast} time={"6 PM"} temp={"19°"} />
-            <HourlyForecastTimeBlock weatherImg={snow} time={"7 PM"} temp={"18°"} />
-            <HourlyForecastTimeBlock weatherImg={fog} time={"8 PM"} temp={"18°"} />
-            <HourlyForecastTimeBlock weatherImg={snow} time={"9 PM"} temp={"17°"} />
-            <HourlyForecastTimeBlock weatherImg={overcast} time={"10 PM"} temp={"17°"} />
-          </div>
+
+
+          <HourlyForecastBlock handleDropdown={handleIsHourlyForecastDropdown} isDropdown={isHourlyForecastDropdown} />
+
         </div>
 
         {/* HOURLY FORECAST DROPDOWN */}
