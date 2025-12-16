9add008502163d578bdc85835f4abd990e74c09f
loganyeh <yehlogan@gmail.com>
2025-12-16 18:32:44 -0500

    feat: wind speed units dropdown toggles

--- src/Components/UnitsDropdown.jsx
+++ src/Components/UnitsDropdown.jsx
@@ -1,6 +1,13 @@
 import colors from "../assets/colors";
+import { useState } from "react";
 
 function UnitsDropdown({ bool }) {
+    const [isMPH, setIsMPH] = useState(false);
+
+    const handleIsMPH = () => {
+      setIsMPH(prev => !prev);
+      console.log(isMPH);
+    }
 
     return(
         <>
@@ -33,11 +40,15 @@ function UnitsDropdown({ bool }) {
             <h3 className="h-7 w-58 flex items-center pl-3 text-base text-gray-400 rounded-lg">
               Wind Speed
             </h3>
-            <h4 className="h-9 w-58 flex justify-between items-center pl-2 text-lg bg-gray-500 items-center rounded-lg hover:border-1 hover:border-white cursor-pointer">
-              km/h<span className="mr-4 text-3xl text-white">O</span>
+            <h4 onClick={handleIsMPH} className={`h-9 w-58 flex justify-between items-center pl-2 text-lg items-center rounded-lg hover:border-1 hover:border-white cursor-pointer
+              ${isMPH ? `` : `bg-gray-500`}
+            `}>
+              km/h<span className={`${isMPH ? `hidden` : ``} mr-4 text-3xl text-white`}>O</span>
             </h4>
-            <h4 className="h-9 w-58 flex justify-start items-center pl-2 text-lg rounded-lg hover:border-1 hover:border-white cursor-pointer">
-              mph
+            <h4 onClick={handleIsMPH} className={`h-9 w-58 flex justify-between items-center pl-2 text-lg items-center rounded-lg hover:border-1 hover:border-white cursor-pointer
+              ${isMPH ? `bg-gray-500` : ``}
+            `}>
+              mph<span className={`${isMPH ? `` : `hidden`} mr-4 text-3xl text-white`}>O</span>
             </h4>
           </div>
           <div className="h-28 w-full flex flex-col justify-around items-center">
